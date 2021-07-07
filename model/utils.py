d495fe2734da3e03c6dec97bfecf9a89cce5e81a
nalinbot <nalinnagar1@gmail.com>
2021-07-07 11:02:08 -0400

    Created utils.py :fire:
    Utility functions for model

---
+++ model/utils.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import numpy as np
+import os
+import sys
+import numpy as np
+from PIL import Image, ImageOps
+
+def get_shape(tensor):
+    return tensor.get_shape().as_list()
+
+def batch_norm(*args, **kwargs):
+
+    with tf.name_scope('bn'):
+        bn = tf.layers.batch_normalization(*args, **kwargs)
+
+    return bn
+
+def lkrelu(x, slope=0.01):
+    return tf.maximum(slope*x, x)
